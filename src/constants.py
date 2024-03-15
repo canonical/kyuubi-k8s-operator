610b59bcbb8f588144d21a4bf7449c4d24465271
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-03-15 18:57:45 +0545

    wip

--- src/constants.py
+++ src/constants.py
@@ -3,5 +3,10 @@
 
 """Literals and constants."""
 
+KYUUBI_CONTAINER_NAME = "kyuubi"
+KYUUBI_SERVICE_NAME = "kyuubi"
+
+S3_INTEGRATOR_REL = "s3-credentials"
+
 NAMESPACE_CONFIG_NAME = "namespace"
 SERVICE_ACCOUNT_CONFIG_NAME = "service-account"
