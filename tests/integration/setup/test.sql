ec60556ba19f264d7aa993c95f7116f3ed01f051
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-03-21 07:35:58 +0545

    Copyright fixes

--- tests/integration/setup/test.sql
+++ tests/integration/setup/test.sql
@@ -1,6 +1,3 @@
--- Copyright 2024 Canonical Ltd.
--- See LICENSE file for licensing details.
-
 CREATE DATABASE IF NOT EXISTS kyuubidb;
 USE kyuubidb;
 CREATE TABLE IF NOT EXISTS kyuubidb.testTable (number Int, word String);
