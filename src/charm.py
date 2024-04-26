2a15bde831f6fe15661031e43a6006506ad7663b
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-04-26 15:39:01 +0200

    fix checks and linting

--- src/charm.py
+++ src/charm.py
@@ -267,10 +267,7 @@ class KyuubiCharm(ops.CharmBase):
 
         raw_info = self.s3_requirer.get_s3_connection_info()
 
-        if any([
-            key not in raw_info
-            for key in ["endpoint", "access-key", "secret-key"]
-        ]):
+        if any(key not in raw_info for key in ["endpoint", "access-key", "secret-key"]):
             return None
 
         return S3ConnectionInfo(
@@ -290,10 +287,7 @@ class KyuubiCharm(ops.CharmBase):
 
         raw_info = self.metastore_db.fetch_relation_data()
         for data in raw_info.values():
-            if any([
-                key not in data
-                for key in ["endpoints", "username", "password"]
-            ]):
+            if any(key not in data for key in ["endpoints", "username", "password"]):
                 continue
             return DatabaseConnectionInfo(
                 endpoint=data["endpoints"],
@@ -312,10 +306,7 @@ class KyuubiCharm(ops.CharmBase):
 
         raw_info = self.auth_db.fetch_relation_data()
         for data in raw_info.values():
-            if any([
-                key not in data
-                for key in ["endpoints", "username", "password"]
-            ]):
+            if any(key not in data for key in ["endpoints", "username", "password"]):
                 continue
             hostname, port = data["endpoints"].split(":")
             return DatabaseConnectionInfo(
