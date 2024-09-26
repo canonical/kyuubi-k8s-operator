a5de5e0bb3336fb1a428134ff1c0ee59a005032d
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-09-26 16:48:41 +0000

    Wip

--- tests/integration/setup/fetch_s3_credentials.sh
+++ tests/integration/setup/fetch_s3_credentials.sh
@@ -12,6 +12,35 @@ get_s3_endpoint(){
     kubectl get service minio -n minio-operator -o jsonpath='{.spec.clusterIP}' 
 }
 
+
+get_s3_access_key(){
+  # Print the S3 Access Key by reading it from K8s secret or by outputting the default value
+    kubectl get secret -n minio-operator microk8s-user-1 &> /dev/null
+    if [ $? -eq 0 ]; then
+        # echo "Use access-key from secret"
+        access_key=$(kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_ACCESS_KEY}' | base64 -d)
+    else
+        # echo "use default access-key"
+        access_key="minio"
+    fi
+    echo "$access_key"
+}
+
+
+get_s3_secret_key(){
+  # Print the S3 Secret Key by reading it from K8s secret or by outputting the default value
+    kubectl get secret -n minio-operator microk8s-user-1 &> /dev/null
+    if [ $? -eq 0 ]; then
+      # echo "Use access-key from secret"
+      secret_key=$(kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_SECRET_KEY}' | base64 -d)
+    else
+      # echo "use default access-key"
+      secret_key="minio123"
+    fi
+    echo "$secret_key"
+}
+
+
 wait_and_retry(){
     # Retry a command for a number of times by waiting a few seconds.
 
@@ -36,6 +65,8 @@ wait_and_retry(){
 # Wait for `minio` service to be ready and S3 endpoint to be available
 wait_and_retry get_s3_endpoint
 S3_ENDPOINT="http://$(get_s3_endpoint)"
+ACCESS_KEY=$(get_s3_access_key)
+SECRET_KEY=$(get_s3_secret_key)
 
 echo $S3_ENDPOINT
 echo $ACCESS_KEY
