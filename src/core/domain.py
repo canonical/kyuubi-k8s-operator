a074716e6d32c045dfccbc31d1ef11e4d4125907
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-01-27 12:32:23 +0000

    Remove unused reference

--- src/core/domain.py
+++ src/core/domain.py
@@ -36,7 +36,6 @@ class Status(Enum):
 
     WAITING_PEBBLE = MaintenanceStatus("Waiting for Pebble")
     MISSING_OBJECT_STORAGE_BACKEND = BlockedStatus("Missing Object Storage backend")
-    INVALID_CREDENTIALS = BlockedStatus("Invalid S3 credentials")
     MISSING_INTEGRATION_HUB = BlockedStatus("Missing integration hub relation")
     INVALID_NAMESPACE = BlockedStatus("Invalid config option: namespace")
     INVALID_SERVICE_ACCOUNT = BlockedStatus("Invalid config option: service-account")
