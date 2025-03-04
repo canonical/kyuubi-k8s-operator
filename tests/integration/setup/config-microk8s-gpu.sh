add9b14dde7807c51fbd9fc9350c11badeaf1e9d
Paolo Sottovia <paolo.sottovia@canonical.com>
2025-03-04 15:03:20 +0000

    Re-enable gpu

--- tests/integration/setup/config-microk8s-gpu.sh
+++ tests/integration/setup/config-microk8s-gpu.sh
@@ -7,20 +7,20 @@ sudo microk8s enable $MICROK8S_ADDONS
 sudo microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
 # # Wait for gpu operator components to be ready
-# while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
-# do
-#     echo "--------------------------------------------------------------------------------------------------------------------"
-#     echo "waiting for validations"
-#     sudo microk8s.kubectl get pods -A
-#     sudo microk8s.kubectl logs -n kube-system -l k8s-app=hostpath-provisioner
-#     sudo microk8s.kubectl describe pod -n gpu-operator-resources nvidia-operator-validator
-#     sudo microk8s.status
-#     sleep 60
+while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
+do
+    echo "--------------------------------------------------------------------------------------------------------------------"
+    echo "waiting for validations"
+    sudo microk8s.kubectl get pods -A
+    sudo microk8s.kubectl logs -n kube-system -l k8s-app=hostpath-provisioner
+    sudo microk8s.kubectl describe pod -n gpu-operator-resources nvidia-operator-validator
+    sudo microk8s.status
+    sleep 60
 
-#     echo "--------------------------------------------------------------------------------------------------------------------"
-# done
+    echo "--------------------------------------------------------------------------------------------------------------------"
+done
 
-sleep 30 # tmp to remove
+# sleep 30 # tmp to remove
 
 # Setup config
 sudo microk8s config > ~/.kube/config
