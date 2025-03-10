aac8c4c601bccce4e5f56abf0a11321f583a57ae
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-03-10 16:11:03 +0000

    Test with new version of integration hub

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -96,7 +96,7 @@ def charm_versions() -> IntegrationTestsCharms:
             **{
                 "name": "spark-integration-hub-k8s",
                 "channel": "latest/edge",
-                "revision": 41,
+                "revision": 42,
                 "series": "jammy",
                 "alias": "integration-hub",
                 "trust": True,
