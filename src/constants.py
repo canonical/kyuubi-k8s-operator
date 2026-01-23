05da14334f96951f2afc0eb5a0aa80ab988b13a4
Alex Batisse <alex.batisse@canonical.com>
2026-01-23 13:06:06 +0100

    fix: Bump spark job OCI resources

--- src/constants.py
+++ src/constants.py
@@ -30,10 +30,10 @@ JDBC_PORT = 10009
 REST_PORT = 10099
 SPARK_DEFAULT_CATALOG_NAME = "spark_catalog"
 
-# spark 3.5.5, release date 12/09/2025
-JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark@sha256:13afe46fec2a84685e92c03fc94d96b52fbeee5c9494c5086537e6de86af512b"
-# spark-gpu 3.5.5, release date 12/09/2025
-GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu@sha256:4ba140bc052089f4a4679e199e99c0f30b16bf6829b62e50bb3d8bef20e67872"
+# spark 3.5.5, release date 16/01/2026
+JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark@sha256:a21e7b2d697fe3cfc61986ac40e60f4e9b6e72c4c2b37b34d3e2a6f46a66a80c"
+# spark-gpu 3.5.5, release date 16/01/2026
+GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu@sha256:06f62f7361f1916aa9f7d27b48428277db690371d2d1acc0dd4ff2674ab75114"
 
 DEFAULT_ADMIN_USERNAME = "admin"
 PASSWORD_SUFFIX = "-password"
