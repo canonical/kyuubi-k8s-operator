9181e74050c6a690a27f8d6c02e5ccc26e7137b0
Alex Batisse <alex.batisse@canonical.com>
2026-01-26 11:20:14 +0100

    ci: Rework GPU integration tests

--- tests/integration/setup/setup-ec2-gpu.sh
+++ tests/integration/setup/setup-ec2-gpu.sh
@@ -4,25 +4,6 @@ set -x
 
 pipx install tox
 pipx ensurepath
-sudo microk8s status --wait-ready
-mkdir ~/.kube
-mkdir ~/workdir
-sudo microk8s config > ~/.kube/config
-sudo microk8s enable hostpath-storage dns rbac nvidia
-sudo microk8s status --wait-ready
-
-while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
-do
-  echo "------------------------------------------------------------------------------------------"
-  echo "waiting for validations"
-  sudo microk8s.kubectl get pods -A
-  sudo microk8s.kubectl logs -n kube-system -l k8s-app=hostpath-provisioner
-  sudo microk8s.kubectl describe pod -n gpu-operator-resources nvidia-operator-validator
-  sudo microk8s.status
-  sleep 60
-  echo "------------------------------------------------------------------------------------------"
-done
-
 
 mkdir -p ~/.local/share/juju
 juju add-k8s mk8s --client
