b4844dbf94b8d9fa9c8d1467923e9a246c36cc88
Alex Batisse <alex.batisse@canonical.com>
2026-01-27 15:28:22 +0100

    ci: Migrate back to microk8s for gpu testing

--- tests/integration/setup/setup-ec2-gpu.sh
+++ tests/integration/setup/setup-ec2-gpu.sh
@@ -5,6 +5,18 @@ set -x
 pipx install tox
 pipx ensurepath
 
+while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
+do
+  echo "------------------------------------------------------------------------------------------"
+  echo "waiting for validations"
+  sudo microk8s.kubectl get pods -A
+  sudo microk8s.kubectl logs -n kube-system -l k8s-app=hostpath-provisioner
+  sudo microk8s.kubectl describe pod -n gpu-operator-resources nvidia-operator-validator
+  sudo microk8s.status
+  sleep 60
+  echo "------------------------------------------------------------------------------------------"
+done
+
 mkdir -p ~/.local/share/juju
 juju add-k8s mk8s --client
 juju bootstrap mk8s mk8s --agent-version 3.6.9
