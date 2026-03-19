56f7a20e44a022a53fe79a6fe83e7bd6c0d2a91b
deusebio <enrico.deusebio@canonical.com>
2026-03-19 22:37:05 +0100

    [PRA-232] Update candidate artifacts (#178)

--- src/constants.py
+++ src/constants.py
@@ -30,10 +30,10 @@ JDBC_PORT = 10009
 REST_PORT = 10099
 SPARK_DEFAULT_CATALOG_NAME = "spark_catalog"
 
-# spark 3.5.7, release date 2026-02-27
-JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark@sha256:c7eef66beaeff463fdb7b707c630c0574912df8b843d8e3169f358adee2ae706"
-# spark-gpu 3.5.7, release date 2026-02-27
-GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu@sha256:8b81961316d19d3d37645c901396876b8e7c3796ef7ca72857ae72d52886d734"
+# spark-3.5.7-22.04, release date 16/03/2026
+JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark@sha256:f7f387a76ba2f3b6cfc34a35f36c0f4ae85bfa78c8c9d4bfa06ba86401b95d70"
+# spark-gpu-3.5.7-22.04, release date 16/03/2026
+GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu@sha256:3df6336efcb1fed300fa25b3725d0ccdaa985ca2a64e686403ad9964ab7a8ed9"
 
 DEFAULT_ADMIN_USERNAME = "admin"
 PASSWORD_SUFFIX = "-password"
