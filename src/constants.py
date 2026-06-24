d33a7c048e3819f1b5a641eac832950e7d32b039
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-06-24 16:13:08 +0545

    Update images to use rapids-26.04.2

--- src/constants.py
+++ src/constants.py
@@ -30,8 +30,8 @@ JDBC_PORT = 10009
 REST_PORT = 10099
 SPARK_DEFAULT_CATALOG_NAME = "spark_catalog"
 
-JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark:3.4-22.04_edge@sha256:af209d4fbd721ba70724233fa940a98e561b6c7d76092ce4b97aef8280e4a23e"  # 3.4-22.04 23-06-2026
-GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu:3.4-22.04_edge@sha256:f2241037d41063e5ed7e8cf4d6dc80c71996f4cd720d470eafc13a73e9db6082"  # 3.4-22.04 23-06-2026
+JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark:3.4-22.04_edge@sha256:7bcf100560677b9ee17b1ff4fe4e1a1cd9aed6db06f30ce5e94cc066b10fcd94"  # 3.4-22.04 24-06-2026
+GPU_JOB_OCI_IMAGE = "ghcr.io/canonical/charmed-spark-gpu:3.4-22.04_edge@sha256:2505b1a26025c2122cb18fcd5f426584790997b2140d6e0d569b5670b4300e44"  # 3.4-22.04 24-06-2026
 
 DEFAULT_ADMIN_USERNAME = "admin"
 PASSWORD_SUFFIX = "-password"
